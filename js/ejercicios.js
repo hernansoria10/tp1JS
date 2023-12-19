23d191dd4f4cc10d33dd90d3c0b3de6dec661346
Hernan Soria <hernansoria02@gmail.com>
2023-12-19 06:58:59 -0300

    se desarrollo con exito el primer ejercicio

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,3 +1,13 @@
 //EJERCICIO 1
-
+document.write('Ejercicio 1 <br>')
+let numero1 = 9;
+let numero2 = 12;
+let suma = numero1 + numero2;
+document.write('La suma entre los numeros: ' + numero1 +' y ' + numero2 + ' es igual a: '+ suma + '<br>');
+let resta = numero1 - numero2;
+document.write('La resta entre los numeros: ' + numero1 +' y ' + numero2 + ' es igual a: '+ resta + '<br>');
+let multiplicacion = numero1 * numero2;
+document.write('La multiplicacion entre los numeros: ' + numero1 +' y ' + numero2 + ' es igual a: '+  multiplicacion + '<br>');
+let division = numero1 / numero2;
+document.write('La division entre los numeros: ' + numero1 +' y ' + numero2 + ' es igual a: ' + (division) + '<br>')
 
