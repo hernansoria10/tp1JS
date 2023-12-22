28bfdfc90fc8f499966c589b9b080069f5128c9b
Hernan Soria <hernansoria02@gmail.com>
2023-12-22 19:44:13 -0300

    se realizo el ejercicio 5

---
+++ js/tp1_Ej_5.js
@@ -0,0 +1,3 @@
+let Numero1 = parseInt(prompt('ingrese el primer numero: '));
+let Numero2 = parseInt(prompt('ingrese el segundo numero: '));
+document.write(Numero1 +' + '+ Numero2 + ' = ' + '<span class="greenBg">'+ (Numero1 + Numero2) + '</span>');
