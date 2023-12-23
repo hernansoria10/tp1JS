0e1ce9018fdb5adfb1f1fd76932d760b06e1513f
Hernan Soria <hernansoria02@gmail.com>
2023-12-22 22:02:54 -0300

    se realizo el ejercicio 8

--- js/tp1_Ej_6.js
+++ js/tp1_Ej_6.js
@@ -1,12 +1,9 @@
-let numero1 = parseInt(prompt('ingrese el primer numero'));
-let numero2 = parseInt(prompt('ingrese el segundo numero'));
-if(numero1>numero2){
-    document.write(numero1 + ' es mayor que '+ numero2);
+let numero1 = parseInt(prompt("ingrese el primer numero"));
+let numero2 = parseInt(prompt("ingrese el segundo numero"));
+if (numero1 > numero2) {
+  document.write(numero1 + " es mayor que " + numero2);
+} else if (numero2 > numero1) {
+  document.write(numero2 + " es mayor que " + numero1);
+} else {
+  document.write("son iguales");
 }
-else if(numero2>numero1){
-    document.write(numero2 + ' es mayor que ' + numero1);
-}
-else{
-    document.write('son iguales');
-}
-
--- js/tp1_Ej_7.js
+++ js/tp1_Ej_7.js
@@ -1,17 +1,13 @@
-let numero1 = parseInt(prompt('ingrese el primer numero'));
-let numero2 = parseInt(prompt('ingrese el segundo numero'));
-let numero3 = parseInt(prompt('ingrese el tercer numero'));
+let numero1 = parseInt(prompt("ingrese el primer numero"));
+let numero2 = parseInt(prompt("ingrese el segundo numero"));
+let numero3 = parseInt(prompt("ingrese el tercer numero"));
 
-if(numero1>numero2 && numero1>numero3){
-    document.write(numero1 + ' es el mayor de los tres ');
+if (numero1 > numero2 && numero1 > numero3) {
+  document.write(numero1 + " es el mayor de los tres ");
+} else if (numero2 > numero1 && numero2 > numero3) {
+  document.write(numero2 + " es el mayor de los tres ");
+} else if (numero3 > numero1 && numero3 > numero2) {
+  document.write(numero3 + " es el mayor de los tres ");
+} else {
+  document.write("son iguales");
 }
-else if(numero2>numero1 && numero2>numero3){
-    document.write(numero2 + ' es el mayor de los tres ');
-}
-else if(numero3>numero1 && numero3>numero2){
-    document.write(numero3 + ' es el mayor de los tres ');
-}
-else{
-    document.write('son iguales');
-}
-
