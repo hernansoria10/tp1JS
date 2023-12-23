ee604afc8386c1624994c1b383b28aa0c841cdb4
Hernan Soria <hernansoria02@gmail.com>
2023-12-22 21:50:12 -0300

    se realizo el ejercicio 7

---
+++ js/tp1_Ej_7.js
@@ -0,0 +1,17 @@
+let numero1 = parseInt(prompt('ingrese el primer numero'));
+let numero2 = parseInt(prompt('ingrese el segundo numero'));
+let numero3 = parseInt(prompt('ingrese el tercer numero'));
+
+if(numero1>numero2 && numero1>numero3){
+    document.write(numero1 + ' es el mayor de los tres ');
+}
+else if(numero2>numero1 && numero2>numero3){
+    document.write(numero2 + ' es el mayor de los tres ');
+}
+else if(numero3>numero1 && numero3>numero2){
+    document.write(numero3 + ' es el mayor de los tres ');
+}
+else{
+    document.write('son iguales');
+}
+
