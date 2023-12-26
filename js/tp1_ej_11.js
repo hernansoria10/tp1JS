f1d18e8ac8daa6426e433fdfde1d4a1c7ec56942
Hernan Soria <hernansoria02@gmail.com>
2023-12-26 17:33:03 -0300

    se realizo el ejercicio 11

--- js/tp1_ej_11.js
+++ js/tp1_ej_11.js
@@ -5,20 +5,53 @@ let div5 = Numero % 5
 let div7 = Numero % 7
 if(div2 == 0){
     document.write(`${Numero} es divisible por 2 `);
-    if (){
-        
+    if (div3 == 0){
+        document.write (`es divisible por 3`)
+    }
+    if (div5 == 0){
+        document.write (`divisible por 5`)
+    }
+    if (div7 == 0){
+        document.write (`y por 7`)  
     }
 }
 else if(div3 == 0){
     document.write(`${Numero} es divisible por 3 `);
+    if (div3 == 0){
+        document.write (`es divisible por 2`)
+    }
+    if (div5 == 0){
+        document.write (`, divisible por 5`)
+    }
+    if (div7 == 0){
+        document.write (`y por 7`)  
+    }
 
 }
 else if(div5 == 0){
     document.write(`${Numero} es divisible por 5 `);
+    if (div3 == 0){
+        document.write (`es divisible por 3`)
+    }
+    if (div5 == 0){
+        document.write (`, divisible por 2`)
+    }
+    if (div7 == 0){
+        document.write (`y por 7`)  
+    }
 
 }
 else if(div7 == 0){
     document.write(`${Numero} es divisible por 7 `);
+    if (div3 == 0){
+        document.write (`es divisible por 3`)
+    }
+    if (div5 == 0){
+        document.write (`divisible por 5`)
+    }
+    if (div7 == 0){
+        document.write (`y por 2`)  
+    }
 
 }
 else{
