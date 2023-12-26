b8d99256202b19df9f6cfa523173e1cc5ef416e3
Hernan Soria <hernansoria02@gmail.com>
2023-12-25 22:26:10 -0300

    creada la estructura de ejercicio 11 a partir de el ejercicio 10

---
+++ js/tp1_ej_11.js
@@ -0,0 +1,26 @@
+let Numero = parseInt(prompt('ingrese un numero'));
+let div2 = Numero % 2
+let div3 = Numero % 3
+let div5 = Numero % 5
+let div7 = Numero % 7
+if(div2 == 0){
+    document.write(`${Numero} es divisible por 2 `);
+    if (){
+        
+    }
+}
+else if(div3 == 0){
+    document.write(`${Numero} es divisible por 3 `);
+
+}
+else if(div5 == 0){
+    document.write(`${Numero} es divisible por 5 `);
+
+}
+else if(div7 == 0){
+    document.write(`${Numero} es divisible por 7 `);
+
+}
+else{
+    document.write('no es divisible por ninguno')
+}
